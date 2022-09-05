b2d27c3a4ff8f3f839dee5ea2b4b1a3392e8e4ab
Nati Schmidt <nati.schmidt97@outlook.de>
2022-09-05 15:53:52 +0200

    .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,9 +14,9 @@ public class Main {
         System.out.println("Finished booting up Calculator 3000");
 //break; was missing in this switch
         boolean inuse = true;
-        while(inuse){
+        while (inuse) {
             int choice = selectInput();
-            switch (choice){
+            switch (choice) {
                 case 1:
                     Addition();
                     break;
@@ -47,81 +47,83 @@ public class Main {
 
         System.out.println("You've chosen  division");
         System.out.println("Enter your numerator: ");
-           float numerator = scanner.nextFloat();
-            System.out.println("Enter your denominator: ");
-            float denominator = scanner.nextFloat();
-            float quotient = numerator / denominator;
-            System.out.println(" The result of " + numerator + " divided by " + denominator + " is " + quotient);
+        float numerator = scanner.nextFloat();
+        System.out.println("Enter your denominator: ");
+        float denominator = scanner.nextFloat();
+        float quotient = numerator / denominator;
+        System.out.println(" The result of " + numerator + " divided by " + denominator + " is " + quotient);
     }
 
-private static void Addition (){
-    System.out.println("You've chosen Addition");
-    System.out.println("Enter your first addend: ");
-    int addend1 = scanner.nextInt();
-    System.out.println("Enter the second addend: ");
-    int addend2 = scanner.nextInt();
-    double sum = Operations.add(addend1,addend2);
-    System.out.println(" The sum of " + addend1 + " and " + addend2 + " is " + sum);
+    private static void Addition() {
+        System.out.println("You've chosen Addition");
+        System.out.println("Enter your first addend: ");
+        int addend1 = scanner.nextInt();
+        System.out.println("Enter the second addend: ");
+        int addend2 = scanner.nextInt();
+        double sum = Operations.add(addend1, addend2);
+        System.out.println(" The sum of " + addend1 + " and " + addend2 + " is " + sum);
 
-}
+    }
 
-    private static void Subtraction (){
+    private static void Subtraction() {
         System.out.println("You've chosen Subtraction");
         System.out.println("Enter the minuend: ");
         int minuend = scanner.nextInt();
         System.out.println("Enter the subtrahend: ");
         int subtrahend = scanner.nextInt();
-        double difference = Operations.subtract(minuend,subtrahend);
+        double difference = Operations.subtract(minuend, subtrahend);
         System.out.println(" The difference of " + minuend + " and " + subtrahend + " is " + difference);
 
     }
 
-    public static void Squareroot(){
+    public static void Squareroot() {
         System.out.println("You've chosen Square root");
         System.out.println("Enter the number: ");
         int int1 = scanner.nextInt();
-        double square  = Operations.sqrt(int1);
+        double square = Operations.sqrt(int1);
         System.out.println("The squareroot of " + int1 + " is " + square);
 
     }
 
-    public static void Multiplication(){
+    public static void Multiplication() {
         System.out.println("You've chosen Multiplication");
         System.out.println("Enter the multiplicand: ");
         int multiplicand = scanner.nextInt();
         System.out.println("Enter the multiplier: ");
         int multiplier = scanner.nextInt();
-        double product  = Operations.multiply(multiplicand,multiplier);
+        double product = Operations.multiply(multiplicand, multiplier);
         System.out.println("The product of" + multiplicand + " and " + multiplier + " is " + product);
 
     }
-// added a mehtod so the throw works
-public static int selectInput() throws InvalidAnswerException {
-    List<Integer> acceptableAnswers = List.of(1, 2, 3,4, 5, 6);
-    System.out.println("Type the corresponding number to select your desired operation: ");
-    System.out.println("1. Addition");
-    System.out.println("2. Subtraction");
-    System.out.println("3. Multiplication");
-    System.out.println("4. Division");
-    System.out.println("5. Square root");
-    System.out.println("6. Exit program");
-    System.out.println(" Your choice: ");
-    int choice = scanner.nextInt();
 
-    try {
-        if (!acceptableAnswers.contains(choice)) {
-            throw new InvalidAnswerException(" Not a valid answer ");
+    // added a mehtod so the throw works
+    public static int selectInput() throws InvalidAnswerException {
+        List<Integer> acceptableAnswers = List.of(1, 2, 3, 4, 5, 6);
+        System.out.println("Type the corresponding number to select your desired operation: ");
+        System.out.println("1. Addition");
+        System.out.println("2. Subtraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. Square root");
+        System.out.println("6. Exit program");
+        System.out.println(" Your choice: ");
+
+        int choice = scanner.nextInt();
+
+        try {
+            if (!acceptableAnswers.contains(choice)) {
+                throw new InvalidAnswerException(" Not a valid answer ");
+            }
+
+        } catch (InvalidAnswerException e) {
+            System.out.println(e.getMessage());
         }
 
-    } catch (InvalidAnswerException e) {
-        System.out.println(e.getMessage());
 
-    }
-
-    return choice;
+            return choice;
 
         }
-        
+
     }
 
 
